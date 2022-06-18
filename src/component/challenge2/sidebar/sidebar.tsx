367825e44d11308c5b326f99c6a0570b3f042c11
MAX-0904 <max@baobabpartners.io>
2022-06-18 18:49:27 +0900

    feat: challenge2 side bar complete

--- src/component/challenge2/sidebar/sidebar.tsx
+++ src/component/challenge2/sidebar/sidebar.tsx
@@ -4,7 +4,11 @@ import styles from "./sidebar.module.scss";
 import Logo from "../logo/logo";
 import Search from "../search/search";
 import NavItem from "../navItem/navItem";
-import {RiDashboardLine} from "react-icons/ri"
+import { RiDashboardLine, RiUser3Line, RiWallet3Line } from "react-icons/ri";
+import { TiDocumentText } from "react-icons/ti";
+import { BiChart } from "react-icons/bi";
+import { FiHelpCircle } from "react-icons/fi";
+import { IoIosNotificationsOutline } from "react-icons/io";
 
 const cn = cb.bind(styles);
 
@@ -19,13 +23,42 @@ const Sidebar: React.FC = () => {
           <Search />
         </div>
         <div className={cn("nav-item__container")}>
-          <NavItem icon={<RiDashboardLine className={cn("icon")} size="2em"/>} name="Dashboard"/>
-          <NavItem icon={<RiDashboardLine className={cn("icon")} size="2em"/>} name="My Profile"/>
-          <NavItem icon={<RiDashboardLine className={cn("icon")} size="2em"/>} name="My Pay"/>
-          <NavItem icon={<RiDashboardLine className={cn("icon")} size="2em"/>} name="My Documents"/>
-          <NavItem icon={<RiDashboardLine className={cn("icon")} size="2em"/>} name="My Retirement"/>
+          <NavItem
+            icon={<RiDashboardLine className={cn("icon")} size="2em" />}
+            name="Dashboard"
+            className={cn("active")}
+          />
+          <NavItem
+            icon={<RiUser3Line className={cn("icon")} size="2em" />}
+            name="My Profile"
+          />
+          <NavItem
+            icon={<RiWallet3Line className={cn("icon")} size="2em" />}
+            name="My Pay"
+          />
+          <NavItem
+            icon={<TiDocumentText className={cn("icon")} size="2em" />}
+            name="My Documents"
+          />
+          <NavItem
+            icon={<BiChart className={cn("icon")} size="2em" />}
+            name="My Retirement"
+          />
         </div>
-        
+      </div>
+      <div className={cn("divider__container")}>
+        <div className={cn("divider")} />
+        <div className={cn("divider--vertical__bar")} />
+      </div>
+      <div className={cn("center__container")}>
+        <NavItem
+          icon={<FiHelpCircle className={cn("icon")} size="2em" />}
+          name="Help Center"
+        />
+        <NavItem
+          icon={<IoIosNotificationsOutline className={cn("icon")} size="2em" />}
+          name="Notifications"
+        />
       </div>
     </div>
   );
